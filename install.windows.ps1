4017c315a04597e9ab3fd5fa37b0c9276f4a02b7
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-12-15 17:07:25 +0000

    d clean and add todo.md
    Co-Authored-By: Tony Trung Thanh Vo <9420482+tonytvo@users.noreply.github.com>

--- install.windows.ps1
+++ install.windows.ps1
@@ -1,5 +1,5 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
-#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/reasonml.starterproject/main/install.windows.ps1 | Invoke-Expression
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/yourlanguage.starterproject/main/install.windows.ps1 | Invoke-Expression
 
 # This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
 #
@@ -9,15 +9,12 @@ iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows
 
 choco install nodejs
 refreshenv
-npm install -g bs-platform
-cinst intellijidea
 
 # Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/reasonml.starterproject.git C:\Code\reasonml.starterproject
-cd C:\Code\reasonml.starterproject
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/yourlanguage.starterproject.git C:\Code\yourlanguage.starterproject
+cd C:\Code\yourlanguage.starterproject
 
-# Run starting reasonml stuff
-npm install
+# initialize and run tests
 run_tests.bat
 
 echo "Done!"
